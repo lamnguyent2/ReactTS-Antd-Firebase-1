aa1e9c7f6316d1498598bb1e624bef9796e07010
lamnguyent2 <lamnguyenxyzt3@gmail.com>
2022-06-27 12:24:43 +0700

    .

--- src/features/Home/components/Main/index.tsx
+++ src/features/Home/components/Main/index.tsx
@@ -6,19 +6,20 @@ export interface IHomeProps { }
 const HomeComponent: React.FC<IHomeProps> = () => {
     return (
         <div className='center-layout'>
-            <a href="" target="_blank" ></a>
-            Danh sách link: <br /><br />
-            Đăng nhập: <a href="/login" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/login</a> <br />
-            Check email: <a href="/check-email" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/check-email</a> <br />
-            Quên mật khẩu: <a href="/forgot-password" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/forgot-password</a> <br />
-            Dashboard: <a href="/dashboard" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/dashboard</a> <br />
-            Quản lý thiết bị: <a href="/device-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/device-management</a> <br />
-            Danh sách dịch vụ: <a href="/service-list" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/service-list</a> <br />
-            Quản lý cấp số: <a href="/number-level-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/number-level-management</a> <br />
-            Quản lý báo cáo: <a href="/report-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/report-management</a> <br />
-            Quản lý vai trò: <a href="/role-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/role-management</a> <br />
-            Danh sách tài khoản: <a href="/account-list" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/account-list</a> <br />
-            Nhật ký người dùng: <a href="/user-logs-list" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/user-logs-list</a>
+            <h2>Danh sách link</h2>
+            <div className="flex-link">
+                <div>Đăng nhập: <a href="/login" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/login</a></div>
+                <div>Check email: <a href="/check-email" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/check-email</a></div>
+                <div>Quên mật khẩu: <a href="/forgot-password" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/forgot-password</a></div>
+                <div>Dashboard: <a href="/dashboard" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/dashboard</a></div>
+                <div>Quản lý thiết bị: <a href="/device-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/device-management</a></div>
+                <div>Danh sách dịch vụ: <a href="/service-list" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/service-list</a></div>
+                <div>Quản lý cấp số: <a href="/number-level-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/number-level-management</a></div>
+                <div>Quản lý báo cáo: <a href="/report-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/report-management</a></div>
+                <div>Quản lý vai trò: <a href="/role-management" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/role-management</a></div>
+                <div>Danh sách tài khoản: <a href="/account-list" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/account-list</a></div>
+                <div>Nhật ký người dùng: <a href="/user-logs-list" target="_blank" >https://react-ts-antd-firebase-1.vercel.app/user-logs-list</a></div>
+            </div>
         </div>
     );
 };
